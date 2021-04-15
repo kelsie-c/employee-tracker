01e1143ee829068cd32160d2ed2269e551d13713
Ghost Orchid <kelsie.cummings23@gmail.com>
2021-04-14 21:45:40 -0400

    added addData function

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
+// require dependencies
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const express = require('express');
 const sequelize = require('./config/connection');
 
+// add middleware
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+// get user & password info and initialize app
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Welcome to the Employee Tracker!'));
-    startApp();
-  });
+    app.listen(PORT, () => {console.log('Welcome to the Employee Tracker!'); startApp();});
+});
 
+// initialize app
 const startApp = () => {
+    // prompt user for first choice
     inquirer.prompt([
         {
             type: 'list',
@@ -20,6 +24,7 @@ const startApp = () => {
             name: 'action'
         }
     ])
+    // call appropriate function based on user selection
     .then(({action}) => {
         if(action === 'Add Data') {
             addData();
@@ -29,4 +34,31 @@ const startApp = () => {
             updateData();
         }
     })
+}
+
+// add data function called from the startApp function
+const addData = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to add?',
+            choices: ['Department', 'Role', 'Employee', 'Go Back'],
+            name: 'addToTable'
+        }
+    ])
+    .then(({addToTable}) => {
+        if(addToTable === 'Department') {
+            addDepartment();
+        } else if(addToTable === 'Role') {
+            addRole();
+        } else if(addToTable === 'Employee') {
+            addEmployee();
+        } else {
+            startApp();
+        }
+    })
+}
+
+const addDepartment = () => {
+    
 }
